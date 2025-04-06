e566299819de79cc55c7f30c86e99fc259e3a191
Dylan Hamersztein <dylanhamersztein@users.noreply.github.com>
2025-04-06 21:22:52 +0100

    Add more parser tests. (#5)

--- src/test/kotlin/org/hamersztein/klox/parser/ParserTest.kt
+++ src/test/kotlin/org/hamersztein/klox/parser/ParserTest.kt
@@ -3,6 +3,7 @@ package org.hamersztein.klox.parser
 import org.hamersztein.klox.ast.expression.Expression
 import org.hamersztein.klox.ast.expression.impl.Binary
 import org.hamersztein.klox.ast.expression.impl.Grouping
+import org.hamersztein.klox.ast.expression.impl.Literal
 import org.hamersztein.klox.ast.expression.impl.Unary
 import org.hamersztein.klox.token.Token
 import org.hamersztein.klox.token.TokenType.*
@@ -17,8 +18,12 @@ import kotlin.test.assertNull
 class ParserTest {
 
     @MethodSource("provideArgsToExpressionInstanceTest")
-    @ParameterizedTest(name = "should create a {1} expression from tokens")
-    fun `should create the right instance of expression`(tokens: List<Token>, expressionClass: KClass<Expression>) {
+    @ParameterizedTest(name = "{0}")
+    fun `should create the right instance of expression`(
+        _ignored: String,
+        tokens: List<Token>,
+        expressionClass: KClass<Expression>
+    ) {
         val parser = Parser(tokens)
 
         val expression = parser.parse()
@@ -41,6 +46,7 @@ class ParserTest {
         @JvmStatic
         fun provideArgsToExpressionInstanceTest() = listOf(
             Arguments.of(
+                "should create a binary expression from equality operator",
                 listOf(
                     Token(NUMBER, "3", 3.0, 1),
                     Token(EQUAL_EQUAL, "==", null, 1),
@@ -50,6 +56,7 @@ class ParserTest {
                 Binary::class
             ),
             Arguments.of(
+                "should create a grouping expression",
                 listOf(
                     Token(LEFT_PAREN, "(", null, 1),
                     Token(NUMBER, "3", 3.0, 1),
@@ -61,20 +68,64 @@ class ParserTest {
                 Grouping::class
             ),
             Arguments.of(
+                "should create a unary expression from a negation",
                 listOf(
-                    Token(BANG, "(", null, 1),
-                    Token(FALSE, "!", false, 1),
+                    Token(BANG, "!", null, 1),
+                    Token(FALSE, "false", false, 1),
                     Token(EOF, "", null, 1),
                 ),
                 Unary::class
             ),
             Arguments.of(
+                "should create a unary expression from a negation of a number",
                 listOf(
                     Token(MINUS, "-", null, 1),
                     Token(NUMBER, "2", 2.0, 1),
                     Token(EOF, "", null, 1),
                 ),
                 Unary::class
+            ),
+            Arguments.of(
+                "should create a literal expression from a string",
+                listOf(
+                    Token(STRING, "\"hello\"", "hello", 1),
+                    Token(EOF, "", null, 1),
+                ),
+                Literal::class
+            ),
+            Arguments.of(
+                "should create a literal expression from a number",
+                listOf(
+                    Token(STRING, "1", 1, 1),
+                    Token(EOF, "", null, 1),
+                ),
+                Literal::class
+            ),
+            Arguments.of(
+                "should create a binary expression from more than one operation",
+                listOf(
+                    Token(NUMBER, "1", 1.0, 1),
+                    Token(PLUS, "+", null, 1),
+                    Token(NUMBER, "2", 2.0, 1),
+                    Token(STAR, "*", null, 1),
+                    Token(NUMBER, "3", 3.0, 1),
+                    Token(EOF, "", null, 1),
+                ),
+                Binary::class
+            ),
+            Arguments.of(
+                "should create a binary expression from more than one grouped operation",
+                listOf(
+                    Token(LEFT_PAREN, "(", null, 1),
+                    Token(NUMBER, "1", 1.0, 1),
+                    Token(PLUS, "+", null, 1),
+                    Token(NUMBER, "2", 2.0, 1),
+                    Token(RIGHT_PAREN, ")", null, 1),
+                    Token(STAR, "*", null, 1),
+                    Token(NUMBER, "3", 3.0, 1),
+                    Token(EOF, "", null, 1),
+                ),
+                Binary::class
             )
         )
 
@@ -86,7 +137,21 @@ class ParserTest {
                     Token(EOF, "", null, 1),
                 )
             ),
-            Arguments.of(listOf(Token(EOF, "", null, 1)))
+            Arguments.of(listOf(Token(EOF, "", null, 1))),
+            Arguments.of(
+                listOf(
+                    Token(NUMBER, "1", 1.0, 1),
+                    Token(PLUS, "+", null, 1),
+                    Token(EOF, "", null, 1),
+                )
+            ),
+            Arguments.of(
+                listOf(
+                    Token(AND, "and", null, 1),
+                    Token(TRUE, "true", true, 1),
+                    Token(EOF, "", null, 1),
+                )
+            )
         )
     }
 
